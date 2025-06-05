b82dfc757efbb2c5219e3f7c491637b8a21422ae
2dbr009 <2dbr009@gmail.com>
2025-06-05 18:31:19 -0300

    Projeto

---
+++ auxiliar.py
@@ -0,0 +1,8 @@
+#arquivo para conseguir a posição do mouse
+
+
+import pyautogui
+import time
+
+time.sleep (5)
+print ((pyautogui.position()))
---
+++ script.py
@@ -0,0 +1,25 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 2
+#enter in your browser
+
+pyautogui.press ('win')
+pyautogui.write ('chrome')
+pyautogui.press ('enter')
+time.sleep (10)
+#enter on emprise system
+#pyautogui.write ("URL site")
+pyautogui.press ("enter")
+
+#wait loading
+time.sleep(5)
+
+
+#make  login
+#pyautogui.click (x = 932, y = 459) #modify of your text input posicion
+pyautogui.write ("your login")
+pyautogui.press ("tab")
+pyautogui.write ("your password")
+pyautogui.press ('tab')
+pyautogui.press ('enter')
